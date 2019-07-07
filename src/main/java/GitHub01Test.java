b0500f7c3b1c3d61945d327dfee7f5c71ec2b71c
bzh <1023487452@qq.com>
2019-07-07 14:05:29 +0800

    java0222

--- src/main/java/GitHub01Test.java
+++ src/main/java/GitHub01Test.java
@@ -2,6 +2,7 @@ public class GitHub01Test {
     public static void main(String[] args) {
         System.out.println("ssssssssssssss");
         System.out.println("我正在测试上传我的第一个GitHub项目");
+        System.out.println("java0222");
 
     }
 
